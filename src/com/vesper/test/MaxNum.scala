fa02363e6e0a60786fb0cdcee5539acc7ff34d2e
vesperj <vesperj@outlook.com>
2017-11-21 18:52:58 +0800

    扫描两次拿到min和max

--- src/com/vesper/test/MaxNum.scala
+++ src/com/vesper/test/MaxNum.scala
@@ -7,23 +7,13 @@ object MaxNum {
     val conf = new SparkConf().setAppName("MaxNum").setMaster("spark://192.168.19.139:7077")
       .setJars(Seq("C:\\Users\\vespe\\IdeaProjects\\sparkstudytest\\out\\artifacts\\sparkstudytest_jar\\sparkstudytest.jar"))
     val sc = new SparkContext(conf)
-//    val lines = sc.textFile("hdfs://192.168.19.139:9000//data//MaxOrMin.log")
-//    lines.map(_.split("\\s+")).reduce((a, b) => if (a > b) a else b)
-    //    val num = lines.map(_.toInt).cache()
-    //    val max = num.reduce((a,b) => if (a > b) a else b)
-    //    val min = num.reduce((a, b) => if (a < b) a else b)
-    //    lines.map(_.toInt).reduce((a, b) => if (a > b) a else b)
-
-    val lines = sc.textFile("hdfs://192.168.19.139:9000//data//MaxOrMin.log").map(_.toInt).reduce((a, b) => if (a > b) a else b)
-
-//    val max = sc.textFile("hdfs://192.168.19.139:9000//data//MaxOrMin.log")
-//      .map(line => line.split(" ").size).reduce((a, b) => if (a > b) a else b)
-//    println(s"max = $max , min = $min")
+    val lines = sc.textFile("hdfs://192.168.19.139:9000//data//MaxOrMin.log")
+    val num = lines.map(_.toInt)
+    val max = num.reduce((a, b) => if (a > b) a else b)
+    val min = num.reduce((a, b) => if (a < b) a else b)
+    println(s"max = $max , min = $min")
     sc.stop()
   }
-//  def fi(a,b) = {
-//    println(s"a=$a , b=$b")
-//    if(a<b) a else b
-//  }
+
 
 }
