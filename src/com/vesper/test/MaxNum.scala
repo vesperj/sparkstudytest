3965abfeeac87cf73166334b4f35f38ae951a34e
vesperj <vesperj@outlook.com>
2017-11-21 19:25:35 +0800

    完整版的MaxOrMin加注了注释信息 半成品的ThreeMax夹住了注释信息 剩余工作为加入信息判断，观察数据的完整性功能

--- src/com/vesper/test/MaxNum.scala
+++ src/com/vesper/test/MaxNum.scala
@@ -2,11 +2,19 @@ package com.vesper.test
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+  * 求出数据中的最大值和最小值
+  * 在集群环境中运行
+  */
 object MaxNum {
   def main(args: Array[String]): Unit = {
+    //设置conf属性，必须指定名字和master节点端口以及本地jar包的位置
     val conf = new SparkConf().setAppName("MaxNum").setMaster("spark://192.168.19.139:7077")
       .setJars(Seq("C:\\Users\\vespe\\IdeaProjects\\sparkstudytest\\out\\artifacts\\sparkstudytest_jar\\sparkstudytest.jar"))
+    //创建sparkContext
     val sc = new SparkContext(conf)
+    //从hdfs上面读取数据源，转变成tuple，
+    // RDD —map—》tuple（后面的参数是一个Int的最大值） —reduce—》比较两组元组中的第一个数字，分为四种情况进行划分
     val lines = sc.textFile("hdfs://192.168.19.139:9000//data//MaxOrMin.log").map(x => (x.toInt, Integer.MAX_VALUE))
       .reduce((x, y) => if (x._1 > y._1 && x._2 > y._1) (x._1, y._1) else if (x._1 < y._1 && x._1 > x._2) (y._1, x._2)
       else if (x._1 < y._1 && x._1 < x._2) (y._1, x._1) else (x._1, x._2))
--- src/com/vesper/test/ThreeMax.scala
+++ src/com/vesper/test/ThreeMax.scala
@@ -2,11 +2,18 @@ package com.vesper.test
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+/**
+  * 找出数据中第三列的数据中的最大值
+  */
 object ThreeMax {
   def main(args: Array[String]): Unit = {
+    //配置conf属性，设置应用名并指定master节点的端口号以及本地jar文件的存放位置
     val conf = new SparkConf().setAppName("ThreeMax").setMaster("spark://192.168.19.139:7077")
       .setJars(Seq("C:\\Users\\vespe\\IdeaProjects\\untitled3\\out\\artifacts\\untitled3_jar\\untitled3.jar"))
+    //创建sparkContext
     val scontext = new SparkContext(conf)
+    //在HDFS上读取文件，并通过map分割后那到第三个数据段，
+    // 之后用reduce进行判断返回最大值
     val lines = scontext.textFile("hdfs://192.168.19.139:9000//data//ThreeMax.log")
       .map(_.split(Array(',',' ','\t'))(2).toInt).reduce((a, b) => if(a>b) a else b)
     println(lines)
